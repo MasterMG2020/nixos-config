f3b6147d526b895eceff977081d59d9aa622b0d3
MasterMG2020 <markus.gaigg2020@gmail.com>
2025-05-09 11:32:23 +0200

    updated some more outdated hyprland configs

--- home-manager/modules/hyprland/modules/decoration.nix
+++ home-manager/modules/hyprland/modules/decoration.nix
@@ -1,15 +1,18 @@
 {
-  wayland.windowManager.hyprland.settings.decoration = {
-    rounding = 10;
-    blur = {
-      enabled = true;
-      size = 3;
-      passes = 6;
+  wayland.windowManager.hyprland.settings = {
+    decoration = {
+      rounding = 10;
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 6;
+      };
+      shadow = {
+        enabled = false;
+        range = 4;
+        render_power = 3;
+        color = "rgba(1a1a1aee)";
+      };
     };
-
-    enabled = false;
-    range = 4;
-    render_power = 3;
-    color = "rgba(1a1a1aee)";
   };
 }
--- home-manager/modules/hyprland/modules/monitors.nix
+++ home-manager/modules/hyprland/modules/monitors.nix
@@ -1,31 +1,34 @@
 { config, ... }: {
-  wayland.windowManager.hyprland.settings.monitor = [
-    # External HDMI monitor (main display, left side)
-    "HDMI-A-1,1920x1080@144,1920x0,1"
-
-    # Internal laptop screen, placed to the right
-    "eDP-1,1920x1080@144,0x0,1"
-
-    # Workspace mapping
-    "workspace=1,monitor:HDMI-A-1"
-    "workspace=2,monitor:HDMI-A-1"
-    "workspace=3,monitor:HDMI-A-1"
-    "workspace=4,monitor:HDMI-A-1"
-    "workspace=5,monitor:HDMI-A-1"
-    "workspace=6,monitor:HDMI-A-1"
-    "workspace=7,monitor:HDMI-A-1"
-    "workspace=8,monitor:HDMI-A-1"
-    "workspace=9,monitor:HDMI-A-1"
-    "workspace=10,monitor:HDMI-A-1"
-    "workspace=11,monitor:eDP-1"
-    "workspace=12,monitor:eDP-1"
-    "workspace=13,monitor:eDP-1"
-    "workspace=14,monitor:eDP-1"
-    "workspace=15,monitor:eDP-1"
-    "workspace=16,monitor:eDP-1"
-    "workspace=17,monitor:eDP-1"
-    "workspace=18,monitor:eDP-1"
-    "workspace=19,monitor:eDP-1"
-    "workspace=20,monitor:eDP-1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      # External HDMI monitor (main display, left side)
+      "HDMI-A-1,1920x1080@144,1920x0,1"
+      # Internal laptop screen, placed to the right
+      "eDP-1,1920x1080@144,0x0,1"
+    ];
+    
+    workspace = [
+      # Workspace mapping
+      "1,monitor:HDMI-A-1"
+      "2,monitor:HDMI-A-1"
+      "3,monitor:HDMI-A-1"
+      "4,monitor:HDMI-A-1"
+      "5,monitor:HDMI-A-1"
+      "6,monitor:HDMI-A-1"
+      "7,monitor:HDMI-A-1"
+      "8,monitor:HDMI-A-1"
+      "9,monitor:HDMI-A-1"
+      "10,monitor:HDMI-A-1"
+      "11,monitor:eDP-1"
+      "12,monitor:eDP-1"
+      "13,monitor:eDP-1"
+      "14,monitor:eDP-1"
+      "15,monitor:eDP-1"
+      "16,monitor:eDP-1"
+      "17,monitor:eDP-1"
+      "18,monitor:eDP-1"
+      "19,monitor:eDP-1"
+      "20,monitor:eDP-1"
+    ];
+  };
 }
